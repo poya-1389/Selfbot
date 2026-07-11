eb085ccb74978e9f96dbf2a756daaccf6a57ca88
poya-1389 <poyaheidary372@gmail.com>
2026-07-11 19:21:01 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
+from database.crud import create_user
 
 from config import BOT_TOKEN
 
