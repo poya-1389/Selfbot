221b762418f8f8031609d9fe85b258ae7bfc7e3d
poya-1389 <poyaheidary372@gmail.com>
2026-07-11 19:23:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import asyncio
 
 from bot.main import main
+from database.create_tables import create_tables
+
+
+async def start():
+
+    await create_tables()
+
+    await main()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start())
